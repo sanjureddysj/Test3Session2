ecd4b9e0ff2fc2d0bb8d6fd46d3e629bda61e668
M SANJEEV <98035905+sanjureddysj@users.noreply.github.com>
2022-03-05 04:53:58 +0000

    p1 debugged

--- p2final.c
+++ p2final.c
@@ -1,3 +1,22 @@
 #include<stdio.h>
 
+struct_fraction
+{
+  int num,den;
+};
+
+typedef struct_fraction Fraction;
+Fraction input_fraction()
+{
+  Fraction f;
+  printf("Enter the fraction\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
+}
+
+Fraction find_smallest(Fraction f2,Fraction f2,Fraction f3)
+{
+  int commmon_denominator =f1
+}
+
 
